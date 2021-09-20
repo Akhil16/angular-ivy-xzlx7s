e204916698c74df08ff7999d48b785bd7bb0e40b
Akhil16 <akhilenduchaturvedi16@gmail.com>
2021-09-20 12:31:00 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,14 @@ export class AppComponent {
   call() {
     if (this.i === 0) {
       this.i = 1;
-      this.router.navigateByUrl(this.router.url + '?key=aaa?');
+      this.router.navigateByUrl('home');
     } else if (this.i === 1) {
       this.i = 2;
-      this.router.navigateByUrl('');
+      this.router.navigateByUrl('home?key=aaa???');
     } else if (this.i === 2) {
       this.i = 0;
-      this.router.navigate([this.router.url], {
-        queryParams: { key: decode('aaa??') },
+      this.router.navigate(['home'], {
+        queryParams: { key: decode('bbb??') },
       });
     }
   }
